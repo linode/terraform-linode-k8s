01cf10651a119b467bb453d2d2fee427d509e8ff
cliedeman <ciaranliedeman@gmail.com>
2018-09-21 17:40:45 +0200

    Added batch

--- scripts/kubeadm-token.sh
+++ scripts/kubeadm-token.sh
@@ -6,7 +6,7 @@ set -e
 eval "$(jq -r '@sh "HOST=\(.host)"')"
 
 # Fetch the join command
-CMD=$(ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
+CMD=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
     root@$HOST kubeadm token create --print-join-command)
 
 # Produce a JSON object containing the join command
