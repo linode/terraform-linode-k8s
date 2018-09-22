5160c70e604e87b0c9dff25f0f95f4812f432927
cliedeman <ciaranliedeman@gmail.com>
2018-09-22 19:14:32 +0200

    Modify /etc/hosts

--- scripts/linode-network.sh
+++ scripts/linode-network.sh
@@ -4,6 +4,8 @@ set -o nounset -o errexit
 NODE_PRIVATE_IP="$1"
 HOSTNAME="$2"
 
+sed  -E '/localhost$/s/(localhost)$/\1 $HOSTNAME/' -i /etc/hosts
+
 echo -e "\nAddress=${NODE_PRIVATE_IP}/17" >> /etc/systemd/network/05-eth0.network
 systemctl daemon-reload
 systemctl restart systemd-networkd
