9b19b9237488448e985e4db8ca8f844255d0dd27
cliedeman <ciaranliedeman@gmail.com>
2018-09-22 19:14:44 +0200

    split remote-execs

--- master.tf
+++ master.tf
@@ -1,26 +1,23 @@
 resource "linode_instance" "k8s_master" {
-  count           = 1
-  region          = "${var.region}"
-  label           = "${terraform.workspace}-master-${count.index + 1}"
-  group           = "${var.linode_group}"
-  type            = "${var.server_type_master}"
-  private_ip      = true
+  count      = 1
+  region     = "${var.region}"
+  label      = "${terraform.workspace}-master-${count.index + 1}"
+  group      = "${var.linode_group}"
+  type       = "${var.server_type_master}"
+  private_ip = true
 
   disk {
-     label           = "boot"
-     size            = 81920
-     authorized_keys = ["${chomp(file(var.ssh_public_key))}"]
-     root_pass       = "${random_string.password.result}"
-     image           = "linode/containerlinux"
-}
-
+    label           = "boot"
+    size            = 81920
+    authorized_keys = ["${chomp(file(var.ssh_public_key))}"]
+    root_pass       = "${random_string.password.result}"
+    image           = "linode/containerlinux"
+  }
 
   config {
     label = "master"
 
-
     kernel = "linode/direct-disk"
-
 
     devices {
       sda = {
@@ -37,6 +34,7 @@ resource "linode_instance" "k8s_master" {
       user = "core"
     }
   }
+
   provisioner "file" {
     source      = "addons/"
     destination = "/tmp"
@@ -45,14 +43,37 @@ resource "linode_instance" "k8s_master" {
       user = "core"
     }
   }
+
   provisioner "remote-exec" {
     # TODO advertise on public adress
     inline = [
       "set -e",
       "chmod +x /tmp/linode-network.sh && sudo /tmp/linode-network.sh ${self.private_ip_address} ${self.label}",
       "chmod +x /tmp/kubeadm-install.sh && sudo /tmp/kubeadm-install.sh ${var.k8s_version} ${var.cni_version} ${self.label}",
-      "sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${self.private_ip_address} --apiserver-cert-extra-sans=${self.ip_address}",
-      "mkdir -p $HOME/.kube && cp -i /etc/kubernetes/admin.conf $HOME/.kube/config",
+    ]
+
+    connection {
+      user = "core"
+    }
+  }
+
+  provisioner "remote-exec" {
+    # TODO advertise on public adress
+    inline = [
+      "set -e",
+      "sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${self.private_ip_address} --apiserver-cert-extra-sans=${self.ip_address} --ignore-preflight-errors=FileExisting-crictl,Service-Docker,FileContent--proc-sys-net-bridge-bridge-nf-call-iptables",
+    ]
+
+    connection {
+      user = "core"
+    }
+  }
+
+  provisioner "remote-exec" {
+    # TODO advertise on public adress
+    inline = [
+      "set -e",
+      "mkdir -p core/.kube && sudo cp -i /etc/kubernetes/admin.conf core/.kube/config && sudo chown core core/.kube/config",
       "export PATH=$${PATH}:/opt/bin",
       "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml",
       "chmod +x /tmp/monitoring-install.sh && /tmp/monitoring-install.sh",
@@ -62,6 +83,7 @@ resource "linode_instance" "k8s_master" {
       user = "core"
     }
   }
+
   provisioner "local-exec" {
     command    = "./scripts/kubectl-conf.sh ${terraform.workspace} ${self.ip_address} ${self.private_ip_address}"
     on_failure = "continue"
