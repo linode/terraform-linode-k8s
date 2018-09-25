8d810d2c301fd2fd236541acfd563625c3842b6a
cliedeman <ciaranliedeman@gmail.com>
2018-09-26 01:09:38 +0200

    fixed kubeconfig copy

--- scripts/kubectl-conf.sh
+++ scripts/kubectl-conf.sh
@@ -7,6 +7,6 @@ PUBLIC_IP=$2
 PRIVATE_IP=$3
 SSH_KEY=$4
 
-scp -i ${SSH_KEY} -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null core@${PUBLIC_IP}:/etc/kubernetes/admin.conf .
+scp -i ${SSH_KEY} -o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null core@${PUBLIC_IP}:/home/core/.kube/config admin.conf
 sed -e "s/${PRIVATE_IP}/${PUBLIC_IP}/g" admin.conf > ${WORKSPACE}.conf
 rm admin.conf
