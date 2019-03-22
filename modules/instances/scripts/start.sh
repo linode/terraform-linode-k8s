af289c573c7d3543c3e98a6007e3a74b9f286c69
Marques Johansson <marques@linode.com>
2019-03-22 07:49:51 -0400

    be more forgiving of container linux state on first run

--- modules/instances/scripts/start.sh
+++ modules/instances/scripts/start.sh
@@ -4,5 +4,7 @@ set -e
 for mod in ip_vs_sh ip_vs ip_vs_rr ip_vs_wrr nf_conntrack_ipv4; do echo $mod | sudo tee /etc/modules-load.d/$mod.conf; done
 
 # Enable the update-engine, but disable the locksmith which it requires
-systemctl unmask update-engine.service && systemctl start update-engine.service
-systemctl stop locksmith.service && systemctl mask locksmith.service
+sudo systemctl unmask update-engine.service || true
+sudo systemctl start update-engine.service || true
+sudo systemctl stop locksmith.service || true
+sudo systemctl mask locksmith.service || true
