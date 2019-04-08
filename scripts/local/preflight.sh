5fa5d5d0576c99d045b2c9b94326da1399a2a20b
Andrew Sauber <a3sauber@gmail.com>
2019-04-08 13:11:39 -0400

    use regex delimiter that doesn't conflict with registry spec

--- scripts/local/preflight.sh
+++ scripts/local/preflight.sh
@@ -20,8 +20,8 @@ rm -rf manifests-tmp
 cp -r manifests manifests-tmp
 
 cp manifests-tmp/ccm-linode.yaml manifests-tmp/ccm-linode.yaml.tmp
-sed -e "s/{{ \.Values\.CCMImage }}/${CCM_IMAGE}/g" manifests-tmp/ccm-linode.yaml.tmp > manifests-tmp/ccm-linode.yaml
+sed -e "s|{{ \.Values\.CCMImage }}|${CCM_IMAGE}|g" manifests-tmp/ccm-linode.yaml.tmp > manifests-tmp/ccm-linode.yaml
 
 cp manifests-tmp/csi-linode.yaml manifests-tmp/csi-linode.yaml.tmp
-sed -e "s/{{ \.Values\.CSIImage }}/${CSI_IMAGE}/g" manifests-tmp/csi-linode.yaml.tmp > manifests-tmp/csi-linode.yaml
+sed -e "s|{{ \.Values\.CSIImage }}|${CSI_IMAGE}|g" manifests-tmp/csi-linode.yaml.tmp > manifests-tmp/csi-linode.yaml
 
