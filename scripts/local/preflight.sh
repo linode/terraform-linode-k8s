883b108f0f7420840470f60bf9d7b28ebf54cdf4
Andrew Sauber <a3sauber@gmail.com>
2019-04-08 12:18:20 -0400

    fix sed escaping for image selection

--- scripts/local/preflight.sh
+++ scripts/local/preflight.sh
@@ -16,10 +16,11 @@ CCM_IMAGE=$1
 CSI_IMAGE=$2
 
 # substitute Docker images in manifests
+rm -r manifests-tmp
 cp -r manifests manifests-tmp
 
 cp manifests-tmp/ccm-linode.yaml manifests-tmp/ccm-linode.yaml.tmp
-sed -e "s/{{ .Values.CCMImage }}/${CCM_IMAGE}/g" manifests-tmp/ccm-linode.yaml.tmp > manifests-tmp/ccm-linode.yaml
+sed -e "s/{{ \.Values\.CCMImage }}/${CCM_IMAGE}/g" manifests-tmp/ccm-linode.yaml.tmp > manifests-tmp/ccm-linode.yaml
 
 cp manifests-tmp/csi-linode.yaml manifests-tmp/csi-linode.yaml.tmp
-sed -e "s/{{ .Values.CSIImage}}/${CSI_IMAGE}/g" manifests-tmp/csi-linode.yaml.tmp > manifests-tmp/csi-linode.yaml
+sed -e "s/{{ \.Values\.CSIImage }}/${CSI_IMAGE}/g" manifests-tmp/csi-linode.yaml.tmp > manifests-tmp/csi-linode.yaml
