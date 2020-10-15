4b188fd44260887262c2ac12e0ac238202098ad4
phillc <15082+phillc@users.noreply.github.com>
2020-10-15 08:51:19 -0400

    Change csi_image for csi_manifest
    CSI version is fairly highly coupled with the manifests

--- variables.tf
+++ variables.tf
@@ -71,8 +71,8 @@ variable "ccm_image" {
   description = "The docker repo/image:tag to use for the CCM"
 }
 
-variable "csi_image" {
+variable "csi_manifest" {
   type        = string
-  default     = "linode/linode-blockstorage-csi-driver:latest"
-  description = "The docker repo/image:tag to use for the CSI"
+  default     = "https://raw.githubusercontent.com/linode/linode-blockstorage-csi-driver/master/pkg/linode-bs/deploy/releases/linode-blockstorage-csi-driver-v0.1.7.yaml"
+  description = "The linode csi manifest location"
 }
